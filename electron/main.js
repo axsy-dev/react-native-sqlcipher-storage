c1d8372ea95586aa3fb865bb0da7f8f90ae619df
Colin Carter <colin.carter@axsy.com>
2025-10-14 16:26:10 +0100

    async-ify

--- electron/main.js
+++ electron/main.js
@@ -1,66 +1,78 @@
 import { ipcMain, app } from "electron";
 import sqlite3 from "@journeyapps/sqlcipher";
-import path from "path";
+import { join, resolve } from "path";
 import { existsSync, unlinkSync } from "fs";
 import log from "electron-log/main";
 
+/**
+ * @import {Database, Statement, RunResult} from "@journeyapps/sqlcipher"
+ *
+ * @typedef {object} RunResultWithRows
+ * @prop {number} lastID
+ * @prop {number} changes
+ * @prop {Array<any>} rows
+ */
+
 class SQLite {
+  /**
+   * @type {Map<string, AsyncDatabase>}
+   * @private
+   */
   #databases = new Map();
+
+  /**
+   * @type {string}
+   * @private
+   */
   #dbLocation = app.getPath("userData");
 
   /**
-   *
    * @param {Event} event
-   * @param {{name: string}} options
-   * @returns {Promise}
+   * @param {{name: string, key: string}} options
+   * @returns {Promise<void>}
    */
   open = async (event, options) => {
     log.info(`open ${JSON.stringify(options)}`);
-    return new Promise((resolve, reject) => {
-      const openPath = path.resolve(path.join(this.#dbLocation, options.name));
-      log.info(`*** sqlite open ${openPath}`);
-
-      const db = new sqlite3.Database(openPath, err => {
-        if (err) {
-          reject(err);
-        } else {
-          this.#databases.set(options.name, db);
-          log.info(`*** sqlite open - success`, db);
-          resolve(db);
-        }
-      });
-    });
+    try {
+      const openPath = resolve(join(this.#dbLocation, options.name));
+      const db = await AsyncDatabase.newDatabase(openPath);
+      if (options.key) {
+        await db.run(`PRAGMA key = '${options.key}'`);
+        await db.run("PRAGMA cipher_migrate");
+      }
+      this.#databases.set(options.name, db);
+    } catch (e) {
+      log.info("Database failed to open");
+      throw e;
+    }
   };
 
   /**
-   *
    * @param {Event} event
    * @param {{path: string}} options
    * @returns {Promise<void>}
    */
   close = async (event, options) => {
     log.info(`*** close - begin`);
-    return new Promise((resolve, reject) => {
-      const db = this.#databases.get(options.path);
-      if (db) {
-        log.info(`*** close ${options.path}`);
-        db.close(err => {
-          if (err) {
-            reject(err);
-          } else {
-            log.info(`*** close - success`);
-            this.#databases.delete(options.path);
-            resolve();
-          }
-        });
-      } else {
-        resolve();
-      }
-    });
+    const db = this.#databases.get(options.path);
+    if (db) {
+      await db.close();
+      this.#databases.delete(options.path);
+    }
   };
 
   /**
-   *
+   * @param {Event} event
+   * @param {{path: string}} options
+   */
+  delete = async (event, options) => {
+    if (this.#databases.has(options.path)) {
+      await this.close(event, options);
+      this.#deleteDatabase(options.path);
+    }
+  };
+
+  /**
    * @param {Event} event
    * @param {{dbargs: {dbname: string}, executes: Array<{qid: string, sql: string, params: any[]}>}} options
    * @returns {Promise<{
@@ -125,79 +137,186 @@ class SQLite {
   };
 
   /**
-   *
-   * @param {sqlite3.Database} db
+   * @param {AsyncDatabase} db
    * @param {string} sql
    * @param {any[]} params
    * @returns {Promise<{rows: any[], rowsAffected: number, insertId: number}>}
    */
-  #all = (db, sql, params) => {
-    return new Promise((resolve, reject) => {
-      db.prepare(sql, function (err) {
-        if (err) {
-          reject(err);
-        } else {
-          if (sql.toLocaleLowerCase().startsWith("select")) {
-            this.all(params, function (err, rows) {
-              if (err) {
-                reject(err);
-              } else {
-                const result = {
-                  rowsAffected: this.changes,
-                  insertId: this.lastID,
-                  rows: rows || []
-                };
-                this.finalize(err => {
-                  if (err) {
-                    reject(err);
-                  } else {
-                    resolve(result);
-                  }
-                });
-              }
-            });
-          } else {
-            this.run(params, function (err) {
-              if (err) {
-                reject(err);
-              } else {
-                const result = {
-                  rowsAffected: this.changes,
-                  insertId: this.lastID
-                };
-                this.finalize(err => {
-                  if (err) {
-                    reject(err);
-                  } else {
-                    resolve(result);
-                  }
-                });
-              }
-            });
-          }
-        }
-      });
-    });
-  };
+  async #all(db, sql, params) {
+    const statement = await db.prepare(sql);
+
+    let result;
+
+    if (sql.toLocaleLowerCase().startsWith("select")) {
+      const all = await statement.all(params);
+      result = {
+        rowsAffected: all.changes,
+        insertId: all.lastID,
+        rows: all.rows
+      };
+    } else {
+      const all = await statement.run(params);
+      result = {
+        rowsAffected: all.changes,
+        insertId: all.insertId
+      };
+    }
+
+    await statement.finalize();
+
+    return result;
+  }
 
   /**
-   *
-   * @param {Event} event
-   * @param {{path: string}} options
+   * @param {string} dbname
    */
-  delete = async (event, options) => {
-    if (this.#databases.has(options.path)) {
-      await this.close(event, options);
-      this.#deleteDatabase(options.path);
-    }
-  };
-
-  #deleteDatabase = dbname => {
-    const dbPath = path.resolve(path.join(this.#dbLocation, dbname));
+  #deleteDatabase(dbname) {
+    const dbPath = resolve(join(this.#dbLocation, dbname));
     if (existsSync(dbPath)) {
       unlinkSync(dbPath);
     }
-  };
+  }
+}
+
+class AsyncDatabase {
+  /**
+   * @type {Database}
+   */
+  #db;
+
+  /**
+   * @param {Database} db
+   */
+  constructor(db) {
+    /**
+     * @type {Database}
+     */
+    this.#db = db;
+  }
+
+  /**
+   * @param {string} openPath
+   * @returns {Promise<AsyncDatabase>}
+   */
+  static newDatabase(openPath) {
+    return new Promise((resolve, reject) => {
+      const db = new sqlite3.Database(openPath, err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(new AsyncDatabase(db));
+        }
+      });
+    });
+  }
+
+  /**
+   * @param {string} sql
+   * @returns {{Promise<RunResult>}}
+   */
+  run(sql) {
+    return new Promise((resolve, reject) => {
+      this.#db.run(sql, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
+    });
+  }
+
+  /**
+   * @returns {Promise<void>}
+   */
+  close() {
+    return new Promise((resolve, reject) => {
+      this.#db.close(err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  /**
+   * @param {string} sql
+   * @returns {AsyncStatement}
+   */
+  prepare(sql) {
+    return new Promise((resolve, reject) => {
+      this.#db.prepare(sql, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(new AsyncStatement(this));
+        }
+      });
+    });
+  }
+}
+
+class AsyncStatement {
+  /**
+   * @type {Statement}
+   */
+  #statement;
+
+  /**
+   * @param {Statement} statement
+   */
+  constructor(statement) {
+    this.#statement = statement;
+  }
+
+  /**
+   * @param {any[]} params
+   * @returns {Promise<RunResultWithRows>}
+   */
+  all(params) {
+    return new Promise((resolve, reject) => {
+      this.#statement.all(params, function (err, rows) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ lastID: this.lastID, changes: this.changes, rows });
+        }
+      });
+    });
+  }
+
+  /**
+   * @param {any[]} params
+   * @returns {Promise<RunResult>}
+   */
+  run(params) {
+    return new Promise((resolve, reject) => {
+      this.#statement.run(params, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
+    });
+  }
+
+  /**
+   * @returns {Promise<void>}
+   */
+  finalize() {
+    return new Promise((resolve, reject) => {
+      this.#statement.finalize(err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
 }
 
 const sqlite = new SQLite();
