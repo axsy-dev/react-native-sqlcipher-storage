7f6be76dd4c087bb974f3061f9b29b2c86f0b3b0
Colin Carter <colin.carter@axsy.com>
2026-03-04 11:13:34 +0000

    remove console.logs

--- electron/preload.js
+++ electron/preload.js
@@ -3,10 +3,8 @@ import { contextBridge, ipcRenderer } from "electron";
 function initSqliteAPI() {
   contextBridge.exposeInMainWorld("sqliteapi", {
     open: async (options, success, error) => {
-      console.log("sqliteapi:open");
       try {
         const result = await ipcRenderer.invoke("sqlite:open", options);
-        console.log("---open", result);
         success(result);
       } catch (e) {
         console.error(e);
@@ -14,10 +12,8 @@ function initSqliteAPI() {
       }
     },
     close: async (options, success, error) => {
-      console.log("sqliteapi:close");
       try {
         const result = await ipcRenderer.invoke("sqlite:close", options);
-        console.log("---close", result);
         success(result);
       } catch (e) {
         console.error(e);
@@ -25,7 +21,6 @@ function initSqliteAPI() {
       }
     },
     delete: async (options, success, error) => {
-      console.log("sqliteapi:delete");
       try {
         await ipcRenderer.invoke("sqlite:delete", options);
         success();
@@ -34,11 +29,10 @@ function initSqliteAPI() {
       }
     },
     backgroundExecuteSqlBatch: async (options, success, error) => {
-      console.log("sqliteapi:backgroundExecuteSqlBatch");
       try {
         const result = await ipcRenderer.invoke(
           "sqlite:backgroundExecuteSqlBatch",
-          options
+          options,
         );
         success(result);
       } catch (e) {
